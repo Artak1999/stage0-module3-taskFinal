7a14b352fccfdaddb9e3460c7a2f902e2cc9491b
Artak1999 <dashyan.1999@mail.ru>
2022-07-05 13:36:00 +0500

    Be careful.

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int result = 0;
+        while(number != 0){
+            int n = number % 10;
+            result += n;
+            number /= 10;
+        }
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result = 0;
+        while(number != 0){
+            int n = number % 10;
+            result = result * 10 + n;
+            number /= 10;
+        }
+        System.out.println(result);
     }
 }
